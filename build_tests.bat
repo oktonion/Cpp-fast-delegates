3f030824e316aac48db49f335ac3e30fa07a8777
oktonion <0oktonion0@gmail.com>
2021-07-19 22:50:50 +0300

    Update build_tests.bat

--- build_tests.bat
+++ build_tests.bat
@@ -3,29 +3,87 @@ mkdir .\tests\obj
 
 setlocal enabledelayedexpansion
 
-set "build_ok=1"
-set INCLUDE=%INCLUDE%
+@ECHO OFF
+
+set true=1==1
+set false=1==0
+
+set build_ok=!true!
+
+set "tests_failed=unsuccessful tests:"
+set has_compile_error=!false!
+set has_compile_warn=!false!
+set current_test_is_ok=!false!
 
 for /f %%f in ('dir /b ".\tests\*.cpp"') do (
-  echo "compiling test %VisualStudioVersion% %%~nf"
-  cl -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -c ".\tests\%%f"
-  IF ERRORLEVEL 1 (
-    set "build_ok=0"
+  echo "compiling test Visual Studio C++ %VisualStudioVersion% %%~nf"
+  set has_compile_error=!false!
+  set has_compile_warn=!false!
+  
+  set "origin_str=%%~nf"
+  set "replaced_str=!origin_str:fail=!"
+  if "!origin_str!"=="!replaced_str!" (
+    cl -nologo -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -c ".\tests\%%f"
+  ) else (
+    cl -nologo -EHsc -W4 -Fo.\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -c ".\tests\%%f" >nul 2>&1
+  )
+  if not !errorlevel!==0 (
+    set has_compile_error=!true!
   )
 
-  if "%build_ok%"=="1" (
-    cl .\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -Fe.\tests\bin\%%~nf.exe
-    IF ERRORLEVEL 1 (
-      set "build_ok=0"
+  set current_test_is_ok=!true!
+
+  if !has_compile_error!==!true! (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=!"
+    if "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      set current_test_is_ok=!false!
+      set "tests_failed=!tests_failed! !origin_str!"
+    ) else (
+      echo "failed as expected"
+    )
+  ) else (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=_!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      set current_test_is_ok=!false!
+      echo "not failed as expected"
+      set "tests_failed=!tests_failed! !origin_str!"
+    )
+  )
+  
+  if !current_test_is_ok!==!true! if !has_compile_error!==!false! (
+    cl -nologo .\tests\obj\%%~nf.obj -D _CRT_SECURE_NO_WARNINGS -Fe.\tests\bin\%%~nf.exe
+    if not !errorlevel!==0 (
+      set has_compile_error=!true!
+    )
+
+    if !has_compile_error!==!true! (
+      set "origin_str=%%~nf"
+      set "replaced_str=!origin_str:fail=!"
+      if "!origin_str!"=="!replaced_str!" (
+        set build_ok=!false!
+        set "tests_failed=!tests_failed! !origin_str!"
+      ) else (
+        echo "failed as expected"
+      )
+    ) else (
+      set "origin_str=%%~nf"
+      set "replaced_str=!origin_str:fail=_!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set build_ok=!false!
+        echo "not failed as expected"
+        set "tests_failed=!tests_failed! !origin_str!"
+      )
     )
   )
 )
 
-if /I "%build_ok%" NEQ "1" (
-  echo "tests build failed"
+if !build_ok!==!false! (
+  echo !tests_failed!
   exit /B 1
 )
-
-echo "tests build ok"
 
 del /Q .\tests\obj\*.obj
