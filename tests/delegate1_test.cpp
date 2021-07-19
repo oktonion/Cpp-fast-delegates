f475ef665423fbd2f73816489d8c82678cd5119e
oktonion <0oktonion0@gmail.com>
2021-07-19 10:57:41 +0300

    Update delegate1_test.cpp

--- tests/delegate1_test.cpp
+++ tests/delegate1_test.cpp
@@ -48,4 +48,42 @@ TEST_CASE("Testing cpp delegate 1") {
 
 		delegate<void, int> dd1(d1);
 	}
+
+	SUBCASE("Delegate 1 copy")
+	{
+        delegate<void, int> d1;
+		d1 = delegate<void, int>(&void_func_int);
+		delegate<void, int> dd1(d1);
+
+		CHECK(d1);
+		CHECK(d1 == dd1);
+
+        void *vptr;
+        int *iptr;
+		
+        d1 = delegate<void, int>(vptr, &void_func_void_p_int);
+
+		CHECK(d1 != dd1);
+	}
+
+	SUBCASE("Delegate 1 invocation")
+	{
+        delegate<void, int> d1;
+		d1 = delegate<void, int>(&void_func_int);
+
+		d1(42);
+		func_called = false;
+		CHECK(true == func_called);
+
+        void *vptr;
+        int *iptr;
+
+        d1 = delegate<void, int>(vptr, &void_func_void_p_int);
+
+		d1(42);
+		func_called = false;
+		CHECK(true == func_called);
+
+		func_called = false;
+	}
 }
