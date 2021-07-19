6f02ee11647842e28f7bf63cecb8192deff54b27
oktonion <0oktonion0@gmail.com>
2021-07-19 23:04:31 +0300

    run tests update

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,18 @@
 run_ok=0
-tests_failed="failed tests:"
+tests_failed="unsuccessful tests:"
 
 for file in ./tests/bin/*; do
   chmod +x "$file"
   filename=$(basename -- "$file")
   filename=$([[ "$filename" = *.* ]] && echo "${filename%.*}" || echo "$filename")
   echo "running test $filename..."
-  $file
+  output=$(($file --duration) 2>&1)
   run_result=$?
   if [ $run_result -eq 0 ]; then
     echo "...ok."
   else
     run_ok=-1
+    echo $output
     echo "...failed with $run_result."
     tests_failed="$tests_failed $filename;"
   fi 
