0e58c980f5c0f600cdafc5b35e048b1abdf6d830
oktonion <0oktonion0@gmail.com>
2021-07-19 22:54:33 +0300

    Update build_tests_bcc32.bat

--- build_tests_bcc32.bat
+++ build_tests_bcc32.bat
@@ -3,31 +3,170 @@ mkdir .\tests\obj
 
 setlocal enabledelayedexpansion
 
-set "build_ok=1"
+@ECHO OFF
 
+set true=1==1
+set false=1==0
 
-for /f %%f in ('dir /b ".\tests\*.cpp"') do (
+set build_ok=!true!
+
+REM The two empty lines are required here
+set "tests_failed=unsuccessful tests:"
+set has_compile_error=!false!
+set has_compile_warn=!false!
+set current_test_is_ok=!false!
+set line_n=0
+
+time /T
+
+for /f  %%f in ('dir /b ".\tests\*.cpp"') do (
   echo "compiling test Borland C++ %%~nf"
-  bcc32 -X- -w-inl -w-ccc -tWC -Q -n.\tests\obj\ -c ".\tests\%%f"
-  IF ERRORLEVEL 1 (
-    set "build_ok=0"
-    exit /B 1
+  set has_compile_error=!false!
+  set has_compile_warn=!false!
+  set /A line_n=0
+  for /f "delims=" %%i in ('bcc32 -X- -w-inl -w-ccc -tWC -Q -n.\tests\obj\ -c ".\tests\%%f"') do (
+    set "MYOUTPUT=%%i"
+    set "origin_str=!MYOUTPUT!"
+    set "replaced_str=!origin_str:Error E=!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_error=!true!
+    )
+    set "replaced_str=!origin_str:Error:=!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_error=!true!
+    )
+    set "replaced_str=!origin_str:Assertion failed=!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_error=!true!
+    )
+    set "replaced_str=!origin_str:Fatal:=!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_error=!true!
+    )
+    set "replaced_str=!origin_str:Internal compiler error=!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_error=!true!
+    )
+    set "replaced_str=!origin_str:Warning W=!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_warn=!true!
+    )
+    set "replaced_str=!origin_str:Warning:=!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set has_compile_warn=!true!
+    )
+    set "ouput_line!line_n!=%%i"
+    set /A line_n=!line_n!+1
+  )
+  
+  set current_test_is_ok=!true!
+
+  if !has_compile_error!==!true! (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=!"
+    if "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      set current_test_is_ok=!false!
+      for /l %%i in (0, 1, !line_n!) do (
+        if defined ouput_line%%i echo !ouput_line%%i!
+      )
+      set "tests_failed=!tests_failed! !origin_str!"
+    ) else (
+      echo "failed as expected"
+    )
+  ) else (
+    set "origin_str=%%~nf"
+    set "replaced_str=!origin_str:fail=_!"
+    if not "!origin_str!"=="!replaced_str!" (
+      set build_ok=!false!
+      set current_test_is_ok=!false!
+      echo "not failed as expected"
+      for /l %%i in (0, 1, !line_n!) do (
+        if defined !ouput_line%%i! echo !ouput_line%%i!
+      )
+      set "tests_failed=!tests_failed! !origin_str!"
+    ) else (
+      if !has_compile_warn!==!true! (
+        for /l %%i in (0, 1, !line_n!) do (
+          if defined ouput_line%%i echo !ouput_line%%i!
+        )
+      )
+    )
   )
 
-  if "%build_ok%"=="1" (
-    bcc32 -X- -w-inl -w-ccc -tWC -Q -lap -e.\tests\bin\%%~nf.exe cw32mt.lib .\tests\obj\%%~nf.obj
-    IF ERRORLEVEL 1 (
-      set "build_ok=0"
-      exit /B 1
+  set line_n=0
+  
+  if !current_test_is_ok!==!true! if !has_compile_error!==!false! (
+    set /A line_n=0
+    for /f "delims=" %%i in ('bcc32 -X- -w-inl -w-ccc -tWC -Q -lap -e.\tests\bin\%%~nf.exe .\tests\obj\%%~nf.obj') do (
+      set "MYOUTPUT=!MYOUTPUT!%%i"
+      set "origin_str=!MYOUTPUT!"
+      set "replaced_str=!origin_str:Error E=!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set has_compile_error=!true!
+      )
+      set "replaced_str=!origin_str:Error:=!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set has_compile_error=!true!
+      )
+      set "replaced_str=!origin_str:Fatal:=!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set has_compile_error=!true!
+      )
+      set "replaced_str=!origin_str:Internal compiler error=!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set has_compile_error=!true!
+      )
+      set "replaced_str=!origin_str:Warning W=!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set has_compile_warn=!true!
+      )
+      set "replaced_str=!origin_str:Warning:=!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set has_compile_warn=!true!
+      )
+      set "ouput_line!line_n!=%%i"
+      set /A line_n=!line_n!+1
+    )
+    
+    if !has_compile_error!==!true! (
+      set "origin_str=%%~nf"
+      set "replaced_str=!origin_str:fail=!"
+      if "!origin_str!"=="!replaced_str!" (
+        set build_ok=!false!
+        for /l %%i in (0, 1, !line_n!) do (
+          if defined ouput_line%%i echo !ouput_line%%i!
+        )
+        set "tests_failed=!tests_failed! !origin_str!"
+      ) else (
+        echo "failed as expected"
+      )
+    ) else (
+      set "origin_str=%%~nf"
+      set "replaced_str=!origin_str:fail=_!"
+      if not "!origin_str!"=="!replaced_str!" (
+        set build_ok=!false!
+        echo "not failed as expected"
+        for /l %%i in (0, 1, !line_n!) do (
+          if defined ouput_line%%i echo !ouput_line%%i!
+        )
+        set "tests_failed=!tests_failed! !origin_str!"
+      ) else (
+        if !has_compile_warn!==!true! (
+          for /l %%i in (0, 1, !line_n!) do (
+            if defined ouput_line%%i echo !ouput_line%%i!
+          )
+        )
+      )
     )
   )
 )
 
-if /I "%build_ok%" NEQ "1" (
-  echo "tests build failed"
+if !build_ok!==!false! (
+  echo !tests_failed!
   exit /B 1
 )
 
-echo "tests build ok"
+time /T
 
 del /Q .\tests\obj\*.obj
